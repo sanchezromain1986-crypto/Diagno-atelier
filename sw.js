69ba70064ddb2027a625797c2f096346f5874e20
sanchezromain1986-crypto <sanchezromain1986@gmail.com>
2026-03-29 18:02:53 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,31 @@
+const CACHE = 'diagno-v2';
+const FILES = [
+  '/Diagno-atelier/',
+  '/Diagno-atelier/index.html',
+  '/Diagno-atelier/manifest.json',
+  '/Diagno-atelier/icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE).then(c => c.put(e.request, clone));
+      return resp;
+    })).catch(() => caches.match('/Diagno-atelier/index.html'))
+  );
+});
